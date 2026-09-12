b83470d35e08ef8af2b1445eacf3e0259eaf5d07
taichi765 <taichi0209.youtub@gmail.com>
2026-09-12 11:22:31 +0900

    feat: Add Context in WorkerThread

--- stern/src/worker.rs
+++ stern/src/worker.rs
@@ -1,22 +1,32 @@
-use std::pin::Pin;
+use std::{pin::Pin, sync::Arc};
 
+use parking_lot::RwLock;
 use tokio::sync::mpsc;
 
 const WORKER_CHANNEL_BUF: usize = 8;
 
 /// A thread to run futures depends on tokio runtime.
-pub struct WorkerThread {
+#[derive(derive_more::Debug)]
+pub struct WorkerThread<C> {
+    #[debug(skip)]
     tx: mpsc::Sender<Pin<Box<dyn Future<Output = ()> + Send + 'static>>>,
+    cx: Arc<RwLock<C>>,
 }
 
 // TODO: 停止できるようにする
-impl WorkerThread {
-    pub fn new() -> Self {
+impl<C> WorkerThread<C>
+where
+    C: Sync + Send + 'static,
+{
+    pub fn new(cx: C) -> Self {
         let (tx, rx) = mpsc::channel(WORKER_CHANNEL_BUF);
         let _join = std::thread::Builder::new()
-            .name("master-worker".to_string())
+            .name("stern-worker".to_string())
             .spawn(move || Self::run(rx));
-        Self { tx }
+        Self {
+            tx,
+            cx: Arc::new(RwLock::new(cx)),
+        }
     }
 
     #[tokio::main]
@@ -27,11 +37,36 @@ impl WorkerThread {
         }
     }
 
+    /// Returns the context of worker.
+    pub fn context(&mut self) -> Arc<RwLock<C>> {
+        Arc::clone(&self.cx)
+    }
+
     pub fn spawn<F>(&self, fut: F)
     where
         F: Future<Output = ()> + Send + 'static,
     {
         self.tx.blocking_send(Box::pin(fut)).unwrap();
+    }
+
+    pub fn spawn_cx<F, Fut>(&self, f: F)
+    where
+        F: FnOnce(Arc<RwLock<C>>) -> Fut + Send + 'static,
+        Fut: Future<Output = ()> + Send + 'static,
+    {
+        let cx = Arc::clone(&self.cx);
+        self.tx
+            .blocking_send(Box::pin(async move { f(cx).await }))
+            .unwrap();
+    }
+}
+
+impl<C> Clone for WorkerThread<C> {
+    fn clone(&self) -> Self {
+        Self {
+            tx: self.tx.clone(),
+            cx: Arc::clone(&self.cx),
+        }
     }
 }
 
