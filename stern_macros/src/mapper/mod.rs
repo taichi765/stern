c6894eab74f9313e9d5d76cd9d7c082176be3630
taichi765 <taichi0209.youtub@gmail.com>
2026-09-18 19:28:35 +0900

    feat: Add define_mapper_impl proc-macro and its snapshot tests

---
+++ stern_macros/src/mapper/mod.rs
@@ -0,0 +1,132 @@
+use proc_macro2::TokenStream;
+use quote::{format_ident, quote};
+use syn::{Expr, Type, TypePath, parse_quote};
+
+use crate::{DefineMapperInput, mapper_trait_ident, mapper_trait_member_type_ident};
+
+pub mod parse;
+
+pub fn generate(input: DefineMapperInput) -> TokenStream {
+    let mapper_trait_name = mapper_trait_ident(&input.base_name.name);
+
+    let impl_items = input.properties.properties.iter().map(|prop| {
+        let type_member_name = mapper_trait_member_type_ident(&prop.name);
+
+        let slint_typ = map_slint_private_type(&prop.slint_typ.typ);
+
+        let domain_typ = prop
+            .domain_typ
+            .as_ref()
+            .map(|v| &v.typ)
+            .unwrap_or(&slint_typ);
+
+        let default_mapper: Expr = parse_quote! {
+            |value: #domain_typ| value
+        };
+        let mapper_expr = prop
+            .mapper
+            .as_ref()
+            .map(|v| &v.expr)
+            .unwrap_or(&default_mapper);
+
+        let map_method_name = format_ident!("map_{}", &prop.name);
+        quote! {
+            type #type_member_name = #domain_typ;
+            fn #map_method_name(value: Self::#type_member_name) -> #slint_typ {
+                (#mapper_expr)(value)
+            }
+        }
+    });
+    quote! {
+        #[derive(Debug, Clone)]
+        pub struct Mapper;
+
+        impl #mapper_trait_name for Mapper {
+            #(#impl_items)*
+        }
+    }
+}
+
+/// Maps `sp::Xxx` to `slint::Xxx`.
+fn map_slint_private_type(typ: &Type) -> Type {
+    match typ {
+        Type::Path(TypePath {
+            attrs: _,
+            qself: _,
+            path,
+        }) => {
+            let mut segs = path.segments.iter();
+            let first = segs.next();
+            if first.is_none() || first.is_some_and(|v| v.ident != "sp") {
+                return typ.clone();
+            };
+            let secound = segs.next();
+            match secound {
+                Some(v) => match v.ident.to_string().as_str() {
+                    "SharedString" => parse_quote!(slint::SharedString),
+                    _ => typ.clone(),
+                },
+                _ => typ.clone(),
+            }
+        }
+        _ => typ.clone(),
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::io::Write;
+
+    use tempfile::NamedTempFile;
+
+    use crate::{PropertyField, generate_mapper_trait};
+
+    use super::*;
+
+    #[test]
+    fn generate_output_is_correct() {
+        let input: DefineMapperInput = syn::parse_str(
+            "base_name: Score,
+            properties: {
+                score: {
+                    domain_typ: u32,
+                    slint_typ: i32,
+                    mapper: {
+                        |value:u32| value.try_into().expect(\"failed to convert u32 into i32\")
+                    },
+                },
+            }",
+        )
+        .unwrap();
+        let output = generate(input);
+        let syn_file = syn::parse_file(output.to_string().as_str()).unwrap();
+        let pretty = prettyplease::unparse(&syn_file);
+        insta::assert_snapshot!(pretty);
+
+        /*let mut file = new_trybuild_file!();
+        file.write_all(pretty.as_bytes()).unwrap();
+        file.write_all("fn main(){}".as_bytes()).unwrap();
+
+        {
+            let mapper_trait = generate_mapper_trait(
+                &format_ident!("Score"),
+                &vec![PropertyField {
+                    ident: format_ident!("score"),
+                    ty: parse_quote!(i32),
+                }],
+            );
+            let syn_file = syn::parse_file(mapper_trait.to_string().as_str()).unwrap();
+            let pretty = prettyplease::unparse(&syn_file);
+            file.write_all(pretty.as_bytes()).unwrap();
+        }
+        let t = trybuild::TestCases::new();
+        t.pass(file.path());*/
+    }
+
+    #[test]
+    fn map_slint_private_type_maps_shared_string() {
+        let priv_typ = parse_quote!(sp::SharedString);
+        let mapped = map_slint_private_type(&priv_typ);
+        assert_eq!(mapped, parse_quote!(slint::SharedString));
+    }
+}
---
+++ stern_macros/src/mapper/parse.rs
@@ -0,0 +1,235 @@
+use syn::{Expr, Ident, Token, Type, braced, parse::Parse, punctuated::Punctuated, token::Brace};
+
+#[derive(Debug, Clone)]
+pub struct DefineMapperInput {
+    pub base_name: BaseName,
+    pub punct: Token![,],
+    pub properties: Properties,
+}
+
+impl Parse for DefineMapperInput {
+    fn parse(input: syn::parse::ParseStream) -> syn::Result<Self> {
+        Ok(Self {
+            base_name: input.parse()?,
+            punct: input.parse()?,
+            properties: input.parse()?,
+        })
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct BaseName {
+    pub base_name_tok: Ident,
+    pub colon: Token![:],
+    pub name: Ident,
+}
+
+impl Parse for BaseName {
+    fn parse(input: syn::parse::ParseStream) -> syn::Result<Self> {
+        let ident: Ident = input.parse()?;
+        if ident != "base_name" {
+            return Err(syn::Error::new_spanned(ident, "expected 'base_name'"));
+        }
+        Ok(Self {
+            base_name_tok: ident,
+            colon: input.parse()?,
+            name: input.parse()?,
+        })
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct Properties {
+    pub properties_ident: Ident,
+    pub colon: Token![:],
+    pub brace: Brace,
+    pub properties: Punctuated<MapperProperty, Token![,]>,
+}
+
+impl Parse for Properties {
+    fn parse(input: syn::parse::ParseStream) -> syn::Result<Self> {
+        let properties_ident: Ident = input.parse()?;
+        if properties_ident != "properties" {
+            return Err(syn::Error::new_spanned(
+                properties_ident,
+                "expected 'properties'",
+            ));
+        }
+        let colon = input.parse()?;
+
+        let content;
+        let brace = braced!(content in input);
+        let properties = content.parse_terminated(MapperProperty::parse, Token![,])?;
+        Ok(Self {
+            properties_ident,
+            colon,
+            brace,
+            properties,
+        })
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct MapperProperty {
+    pub name: Ident,
+    pub colon: Token![:],
+    pub brace: Brace,
+    pub domain_typ: Option<DomainTypEntry>,
+    pub slint_typ: SlintTypEntry,
+    pub mapper: Option<MapperEntry>,
+}
+
+impl Parse for MapperProperty {
+    fn parse(input: syn::parse::ParseStream) -> syn::Result<Self> {
+        let name: Ident = input.parse()?;
+        let colon: Token![:] = input.parse()?;
+
+        let content;
+        let brace = braced!(content in input);
+        let prop_infos = content.parse_terminated(PropertyInfo::parse, Token![,])?;
+
+        let mut domain_typ = None;
+        let mut slint_typ = None;
+        let mut mapper = None;
+        for info in prop_infos {
+            match info {
+                PropertyInfo::DomainTyp(v) => {
+                    if domain_typ.is_some() {
+                        return Err(syn::Error::new_spanned(
+                            v.ident,
+                            "found multiple entry of 'domain_typ'",
+                        ));
+                    }
+                    domain_typ = Some(v)
+                }
+                PropertyInfo::SlintTyp(v) => {
+                    if slint_typ.is_some() {
+                        return Err(syn::Error::new_spanned(
+                            v.ident,
+                            "found multiple entry of 'slint_typ'",
+                        ));
+                    }
+                    slint_typ = Some(v)
+                }
+                PropertyInfo::Mapper(v) => {
+                    if mapper.is_some() {
+                        return Err(syn::Error::new_spanned(
+                            v.ident,
+                            "found multiple entry of 'mapper'",
+                        ));
+                    }
+                    mapper = Some(v)
+                }
+            }
+        }
+
+        if (domain_typ.is_some() && mapper.is_none()) || (domain_typ.is_none() && mapper.is_some())
+        {
+            return Err(syn::Error::new_spanned(
+                name.clone(),
+                "only one of 'domain_typ' and 'mapper' is passed as input. you need to specify both of them or none of them.",
+            ));
+        }
+
+        Ok(Self {
+            name: name.clone(),
+            colon,
+            brace,
+            domain_typ: domain_typ,
+            slint_typ: slint_typ.ok_or(syn::Error::new_spanned(
+                name.clone(),
+                "no entry of 'slint_typ' found",
+            ))?,
+            mapper,
+        })
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct DomainTypEntry {
+    pub ident: Ident,
+    pub colon: Token![:],
+    pub typ: Type,
+}
+
+#[derive(Debug, Clone)]
+pub struct SlintTypEntry {
+    pub ident: Ident,
+    pub colon: Token![:],
+    pub typ: Type,
+}
+
+#[derive(Debug, Clone)]
+pub struct MapperEntry {
+    pub ident: Ident,
+    pub colon: Token![:],
+    pub expr: Expr,
+}
+
+#[derive(Debug, Clone)]
+pub enum PropertyInfo {
+    DomainTyp(DomainTypEntry),
+    SlintTyp(SlintTypEntry),
+    Mapper(MapperEntry),
+}
+
+impl Parse for PropertyInfo {
+    fn parse(input: syn::parse::ParseStream) -> syn::Result<Self> {
+        let ident: Ident = input.parse()?;
+        let colon: Token![:] = input.parse()?;
+        match ident.to_string().as_str() {
+            "domain_typ" => Ok(Self::DomainTyp(DomainTypEntry {
+                ident,
+                colon,
+                typ: input.parse()?,
+            })),
+            "slint_typ" => Ok(Self::SlintTyp(SlintTypEntry {
+                ident,
+                colon,
+                typ: input.parse()?,
+            })),
+            "mapper" => Ok(Self::Mapper(MapperEntry {
+                ident,
+                colon,
+                expr: input.parse()?,
+            })),
+            _ => Err(syn::Error::new_spanned(
+                ident,
+                "unexpected property info field",
+            )),
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn define_mapper_input_parses_correctly() {
+        let got: DefineMapperInput = syn::parse_str(
+            "base_name: Score,
+            properties:{
+            player_name: {
+            domain_typ: String, slint_typ: slint::SharedString, mapper: {
+            value.to_shared_string()
+            }
+            },count:{domain_typ: u32, slint_typ:i32,mapper:{value.try_into().unwrap()}}}",
+        )
+        .unwrap();
+    }
+
+    #[test]
+    fn mapped_property_parses_correctly() {
+        let got: MapperProperty = syn::parse_str(
+            "player_name: {
+            domain_typ: String,
+            slint_typ: slint::SharedString,
+            mapper: {
+                value.to_shared_string()
+            }
+        }",
+        )
+        .unwrap();
+    }
+}
